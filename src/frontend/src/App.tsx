97b6c31c6042da724e612411fd13116ee6aa2016
Zero Ash <admin@remiliascarlet.com>
2026-01-09 03:59:42 +0800

    feat: hendling route from 404 page

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,9 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 import VideoRating from "@/pages/video-rating";
 import LolDice from "@/pages/lol-dice";
 
 function App() {
+  const redirector = useNavigate();
+
+  useEffect(() => {
+    const redirectTarget = window.sessionStorage.getItem("redirect");
+
+    if (redirectTarget) {
+      window.sessionStorage.removeItem("redirect");
+      redirector(redirectTarget);
+    }
+  }, []);
+
   return (
     <Routes>
       <Route element={<VideoRating />} path="/" />
